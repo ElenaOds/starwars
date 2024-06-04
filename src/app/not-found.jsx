0f71bd2916087a7ce2739143b4730bdd33b1a476
Elena Pedko <elpg.ods@gmail.com>
2024-06-04 18:30:35 +0300

    spelling mistake is corrected

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
     <div className="h-screen pt-10 md:pb-10 lg:pb-44 px-10 flex flex-col">
-        <h2 className="text-white mb-5 self-center">Ups...Requsted page not found</h2>
+        <h2 className="text-white mb-5 self-center">Ups...Requested page not found</h2>
         <Link href="/" className="text-blue-500 self-center underline underline-offset-1 hover:text-amber-400">
         Back to Home page
       </Link>
